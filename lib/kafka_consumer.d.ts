5ca1e588f9c803898f6f568b7ef7cab155a6913e
Deivi Pena <dpena@bankingtech.com.ve>
2022-04-10 19:50:36 -0400

    Adding kafka consumer-producer funcionality

---
+++ lib/kafka_consumer.d.ts
@@ -0,0 +1,21 @@
+import { Consumer, EachMessagePayload } from "kafkajs";
+export interface ConsumerContract {
+    groupId: string;
+    topic: string;
+    base: Consumer;
+    handler(payload: EachMessagePayload): Promise<void>;
+    prepareConsumer(): Promise<void>;
+    doHandle(): void;
+    error(error: Error): void;
+}
+export declare abstract class BaseConsumer implements ConsumerContract {
+    base: Consumer;
+    groupId: string;
+    topic: string;
+    constructor(base: Consumer);
+    prepareConsumer(): Promise<void>;
+    abstract handler(payload: EachMessagePayload): Promise<void>;
+    doHandle(): void;
+    error(error: Error): void;
+}
+//# sourceMappingURL=kafka_consumer.d.ts.map
